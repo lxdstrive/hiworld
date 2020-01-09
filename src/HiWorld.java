4be134bc2ae5e06ef5d02b8ec35036ebdaba2087
lxdstrive <lxdstrive@163.com>
2020-01-09 23:12:44 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author lxd
+ * @date 2020/1/9 23:11
+ * @description
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("hiworld");
+    }
+}
